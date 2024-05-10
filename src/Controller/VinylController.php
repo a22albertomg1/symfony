49957246cdcf2392ce8fb1c0819cb4a40e5401eb
a22albertomg1 <a22albertomg1@iessanclemente.net>
2024-05-10 13:56:36 +0200

    Added some Twiggy goodness

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['cancion' => 'La Flaca', 'artista' => 'Jarabe de Palo'],
+            ['cancion' => 'La Camisa Negra', 'artista' => 'Juanes'],
+            ['cancion' => 'Vivir Mi Vida', 'artista' => 'Marc Anthony'],
+            ['cancion' => 'A Dios le Pido', 'artista' => 'Juanes'],
+            ['cancion' => 'Volare', 'artista' => 'Gipsy Kings'],
+            ['cancion' => 'La Bamba', 'artista' => 'Ritchie Valens'],
+            ['cancion' => 'Clandestino', 'artista' => 'Manu Chao'],
+            ['cancion' => 'La Cumparsita', 'artista' => 'Carlos Gardel'],
+            ['cancion' => 'La Macarena', 'artista' => 'Los del Río']
+        ];
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}')]
