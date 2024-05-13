8b93d55f97c4cc48c29a58d74594658fe8ba6138
a22albertomg1 <a22albertomg1@iessanclemente.net>
2024-05-13 14:27:56 +0200

    Look mom! A real app

---
+++ src/Controller/SongController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SongController extends AbstractController
+{
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
+    public function getSong(int $id, LoggerInterface $logger): Response
+    {
+        // TODO query the database
+        $song = [
+            'id' => $id,
+            'name' => 'La Flaca',
+            'url' => 'https://youtu.be/r2g0pM3PMNQ?si=Ro19pxuORa8Yaw_4'
+        ];
+
+        $logger->info('Returning API response for song {song}', [
+            'song' => $id,
+        ]);
+        return $this->json($song);
+    }
+}
